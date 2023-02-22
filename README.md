cc3ea66f1a22131ae39edd50592cc94bf8098bb5
Sebastian Rojas <jhsrojasro@unal.edu.co>
2023-02-22 14:06:38 +0100

    MongoDB authentication et variables d'environement

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # user_ms
 AnimAIxpert's User Microservice
+
+To install dependencies:
+
+pip install -r requirements.txt
+
+
+To run:
+
+flask --app app run
+
+
+Create user in mongodb:
+
+db.createUser({
+    user: "animaixpert",
+    pwd: passwordPrompt(),
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+    }
+)
--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
+load_dotenv()
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient(
+    os.environ.get('MONGO_HOST'),
+    int(os.environ.get('MONGO_PORT')),
+    username=os.environ.get('MONGO_USERNAME'),
+    password=os.environ.get('MONGO_PASSWORD')    
+)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return os.environ.get('MONGO_USERNAME')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
